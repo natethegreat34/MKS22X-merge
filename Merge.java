e1eb4d32ae93c26033ff6fefc303440fc6b07833
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2019-03-15 13:16:31 -0400

    Am I done?
    Accidentally added to the quick lab

--- Merge.java
+++ Merge.java
@@ -1,6 +1,8 @@
-public class Merge{
+rpublic class Merge{
     /*sort the array from least to greatest value. This is a wrapper function*/
     public static void mergesort(int[]data){
+        int lo = 0;
+        int hi = data.length -1;
         mergesort(data,lo,hi);
       if (lo < hi){
         int [] left;
@@ -14,14 +16,30 @@ public class Merge{
         mergesort (left);
         mergesort (right);
     }
-    merge (left, right);
+    merge (left, right, data);
 }
-    private static void merge(int[]data, int [] copy){
+    private static void merge(int[] left, int [] right, int [] original){
+        int l = 0;
         int r = 0;
-        int c = 0;
-        for (; r <)
-        ;
+        for (;i < original.length; i ++){
+            if (l < left.length && r < right.length){
+                 if (left [l] <= left [r]){
+                    original [i] = left [l];
+                    l ++;
+                }
+                if (left [r] < left [l]){
+                    original [i] = left [r];
+                    r ++;
+                }
+            }
+            if (l >= left.length && r < right.length){
+                original [i] = left [r];
+                r ++;
+            }
+            if (r >= right.length && l < left.length){
+                original [i] = left [l];
+                l ++;
+            }
+        }
     }
-
-
 }
