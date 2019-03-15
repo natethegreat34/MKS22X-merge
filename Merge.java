d197f1066dc3ecd46f1210778cadb63d9164b0b4
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2019-03-15 10:11:37 -0400

    working on it

---
+++ Merge.java
@@ -0,0 +1,27 @@
+public class Merge{
+    /*sort the array from least to greatest value. This is a wrapper function*/
+    public static void mergesort(int[]data){
+        mergesort(data,lo,hi);
+      if (lo < hi){
+        int [] left;
+        for (int y = lo; y < (hi+lo)/2; y ++){
+            left [y] = data [y];
+        }
+        int [] right;
+        for (int y = (hi+lo)/2; y < hi + 1; y ++){
+            right [y] = data [y];
+        }
+        mergesort (left);
+        mergesort (right);
+    }
+    merge (left, right);
+}
+    private static void merge(int[]data, int [] copy){
+        int r = 0;
+        int c = 0;
+        for (; r <)
+        ;
+    }
+
+
+}
