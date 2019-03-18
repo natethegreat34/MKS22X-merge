5be1d0e784eaccdab93adcbc3adabd170af7e370
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2019-03-18 09:45:17 -0400

    Trying to understand my code

--- Merge.java
+++ Merge.java
@@ -1,27 +1,36 @@
-rpublic class Merge{
+public class Merge{
     /*sort the array from least to greatest value. This is a wrapper function*/
     public static void mergesort(int[]data){
         int lo = 0;
         int hi = data.length -1;
+        int mid = data.length / 2;
+        // sets the start and end of the given array
+        int [] left = new int [mid];
+        int [] right = new int [mid];
+        //makes tow arrays, by splitting the current one into a right and left one
         mergesort(data,lo,hi);
       if (lo < hi){
-        int [] left;
-        for (int y = lo; y < (hi+lo)/2; y ++){
+        for (int y = lo; y < mid; y ++){
             left [y] = data [y];
         }
-        int [] right;
-        for (int y = (hi+lo)/2; y < hi + 1; y ++){
+        //for left array, loop from 0 to half of the array - 1 and assgn each value to the space of left
+
+        for (int y = mid; y < hi + 1; y ++){
             right [y] = data [y];
         }
+        //for right array, loop from half of the array to end of the array and assgn each value to the space of right
+
         mergesort (left);
         mergesort (right);
+        // recursive
     }
     merge (left, right, data);
+    // combine all lonely arrays
 }
     private static void merge(int[] left, int [] right, int [] original){
         int l = 0;
         int r = 0;
-        for (;i < original.length; i ++){
+        for (int i = 0 ;i < original.length; i ++){
             if (l < left.length && r < right.length){
                  if (left [l] <= left [r]){
                     original [i] = left [l];
