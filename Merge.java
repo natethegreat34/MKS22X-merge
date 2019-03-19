c5d87ac5cf92eda58803d1b2062a130346869fb6
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2019-03-19 07:25:28 -0400

    Debugging

--- Merge.java
+++ Merge.java
@@ -1,33 +1,35 @@
 public class Merge{
-
 public static void mergesort(int[]data){
-	int mid = data.length / 2;
-	int lo = 0;
-	int hi = data.length;
+	mergesort(data, 0, data.length - 1);
+}
+public static void mergesort(int[]data, int lo, int hi){
+	int mid = (hi + lo) / 2;
 	//makes tow arrays, by splitting the current one into a right and left one
 	// mergesort(data,lo,hi);
-	if (lo >= hi || data.length < 2 || mid == lo){
+	if (lo >= hi || hi - lo < 2 || mid == lo || data.length < 2){
 		System.out.println("smxhiuxbsbgdjs");
 		return;
 	}
-	int [] left = new int [mid];
-	for (int y = lo; y < mid; y ++){
+	int [] left = new int [mid + 1 - lo];
+	for (int y = 0; y <= mid - lo; y ++){
+		System.out.println("place:" + y) ;
+				System.out.println("length:" + data.length);
 		left [y] = data [y];
 	}
 	//for left array, loop from 0 to half of the array - 1 and assgn each value to the space of left
- 	int [] right = new int [hi - mid + 1];
+ 	int [] right = new int [hi - mid];
 
-	for (int y = mid; y < hi; y ++){
+	for (int y = 1 + mid - lo; y < hi- lo; y ++){
 		// System.out.println(mid);
 		// System.out.println(y + "y");
 		// System.out.println(right.length);
-		right [y - mid] = data [y];
+		right [y - mid - 1 + lo] = data [y];
 	}
 	//for right array, loop from half of the array to end of the array and assgn each value to the space of right
 // System.out.println("pb");
-	mergesort (left);
+	mergesort (left, lo, mid);
 	// System.out.println("bj");
-	mergesort (right);
+	mergesort (right, mid + 1, hi);
 	// recursive
 	// System.out.println("saleeehfjdjfhjdh");
 	merge (left, right, data);
