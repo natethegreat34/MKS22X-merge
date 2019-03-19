49111100fea4b28422bbf1e9483298a2c4f51660
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2019-03-18 23:21:10 -0400

    Trying

--- Merge.java
+++ Merge.java
@@ -9,56 +9,76 @@
 	    }
 	    /*sort the array from least to greatest value. This is a wrapper function*/
 	    public static void mergesort(int[]data){
-	       if (data.length > 0){
-	        int lo = 0;
-	        int hi = data.length -1;
-	        int mid = data.length / 2;
-	        // sets the start and end of the given array
-	        int [] left = new int [mid];
-	        int [] right = new int [mid];
-	        //makes tow arrays, by splitting the current one into a right and left one
-	        // mergesort(data,lo,hi);
-            if (lo < hi){
-	        for (int y = lo; y < mid; y ++){
-	            left [y] = data [y];
-	        }
-	        //for left array, loop from 0 to half of the array - 1 and assgn each value to the space of left
-
-	        for (int y = mid; y < hi + 1; y ++){
-	            right [y - mid] = data [y];
-	        }
-	        //for right array, loop from half of the array to end of the array and assgn each value to the space of right
-
-	        mergesort (left);
-	        mergesort (right);
+			if (data.length > 0)
+			mergesort(data, 0, data.length);
+		}
+		public static void mergesort(int[]data, int lo, int hi){
+		int mid = (lo + hi)/ 2;
+	       if (lo < hi){
+	        mergesort (data, lo,mid );
+	        mergesort (data, mid + 1, hi);
 	        // recursive
 	    }
-	    merge (left, right, data);
+	    merge (data, lo, hi);
 	       }
 	    // combine all lonely arrays
-	}
-	    private static void merge(int[] left, int [] right, int [] original){
+	    private static void merge(int[] data, int lo, int hi){
+		   int mid = (lo + hi)/ 2;
+		   // sets the start and end of the given array
+		   int [] left = new int [mid];
+		   int [] right = new int [mid];
+		   //makes tow arrays, by splitting the current one into a right and left one
+		   // mergesort(data,lo,hi);
+			if (lo < hi){
+		   for (int y = lo; y < mid; y ++){
+			   left [y] = data [y];
+		   }
+		   //for left array, loop from 0 to half of the array - 1 and assgn each value to the space of left
+
+		   for (int y = mid; y - mid >= 0 && y < hi + 1; y ++){
+			   right [y - mid] = data [y];
+		   }
+		   //for right array, loop from half of the array to end of the array and assgn each value to the space of right
+
 	        int l = 0;
 	        int r = 0;
-	        for (int i = 0 ;i < original.length; i ++){
-                if (l < left.length && r < right.length){
-	                 if (left [l] <= right [r]){
-	                    original [i] = left [l];
-	                    l ++;
-	                }
-	                else if(right [r] < left [l]){
-	                    original [i] = right [r];
-	                    r ++;
-	                }
-	            }
-	            if (l >= left.length && r < right.length){
-	                original [i] = right [r];
-	                r ++;
-	            }
-	            if (r >= right.length && l < left.length){
-	                original [i] = left [l];
-	                l ++;
-	            }
+			int i = 0;
+			for(; l < left.length && r < right.length; i ++){
+				if (left [l] <= right [r]){
+		             data [i] = left [l];
+		             l ++;
+		        }
+				if (left [l] > right [r]){
+		             data [i] = right [r];
+		             r ++;
+		        }
+
+			}
+			while (r < right.length){
+				data [i] = right [r];
+			   r ++;
+		  	}
+			while (l < right.length){
+				data [i] = right [l];
+			   l ++;
+		  	}
+
+			}
+	        // for ( ;i < original.length ; i ++){
+            //     if (l < left.length && r < right.length){
+	        //
+	        //         else if(right [r] < left [l]){
+	        //             original [i] = right [r];
+	        //             r ++;
+	        //         }
+	        //     }
+	        //     if (l >= left.length && r < right.length){
+	        //         original [i] = right [r];
+	        //         r ++;
+	        //     }
+	        //     if (r >= right.length && l < left.length){
+	        //         original [i] = left [l];
+	        //         l ++;
+	        //     }
 	        }
 	    }
-	}
