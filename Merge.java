1be31d92a9d5a4c72c48bbea670602d323be6ef9
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2019-03-19 22:24:13 -0400

    Works for size 3

--- Merge.java
+++ Merge.java
@@ -1,11 +1,33 @@
 public class Merge{
+		private static  int [] original;
 public static void main(String[] args) {
-	int [] a = {1,4,7,6,5};
+	int [] a = {6, 7 , 5};
 	mergesort (a);
-	System.out.println(a);
-}
+	// not changing a
+    String d = "";
+	 for (int x = 0; x < a.length; x ++){
+        d = d + a[x] + " , ";
+      }
+System.out.println(d);
+	}
+
 public static void mergesort(int[]data){
-	mergesort(data, 0, data.length - 1);
+	int [] copy = new int [data.length];
+
+	for (int x = 0; x < data.length; x ++){
+		copy [x] = data [x];
+	}
+	// System.out.println(d);
+	// original [0] = 10;
+ 	// d = "";
+	// for (int x = 0; x < data.length; x ++){
+	// 	d = d + data[x] + " , ";
+	// }
+	// System.out.println(d + "after");
+	mergesort(copy, 0, data.length - 1);
+	for (int x = 0; x < original.length; x ++){
+		data [x] = original [x];
+	}
 }
 //		 /
 //  	/
@@ -15,14 +37,12 @@ public static void mergesort(int[]data, int lo, int hi){
 	int mid = (hi + lo) / 2;
 	//makes tow arrays, by splitting the current one into a right and left one
 	// mergesort(data,lo,hi);
-	if (lo >= hi || hi - lo < 2 || mid == lo || data.length < 2){
-		System.out.println("smxhiuxbsbgdjs");
+	if (lo >= hi || data.length < 2){
+		// System.out.println("smxhiuxbsbgdjs");
 		return;
 	}
 	int [] left = new int [mid + 1 - lo];
 	for (int y = 0; y <= mid - lo; y ++){
-		System.out.println("place:" + y) ;
-				System.out.println("length:" + data.length);
 		left [y] = data [y];
 	}
 	//for left array, loop from 0 to half of the array - 1 and assgn each value to the space of left
@@ -41,37 +61,40 @@ public static void mergesort(int[]data, int lo, int hi){
 	mergesort (right, mid + 1, hi);
 	// recursive
 	// System.out.println("saleeehfjdjfhjdh");
-	merge (left, right, data);
+	merge (left, right);
+	// String d = "";
+	// for (int x = 0; x < data.length; x ++){
+	// 	d = d + data[x] + " , ";
+	// }
+	// System.out.println(d + "after");
    }
-public static void merge(int[] left, int [] right, int [] data){
-	data = new int [left.length + right.length];
+public static void merge(int[] left, int [] right){
+	original = new int [left.length + right.length];
 	int l = 0;
 	int r = 0;
 	int i = 0;
-	while(l < left.length && r < right.length && i < data.length){
+	while(l < left.length && r < right.length && i < original.length){
 			if (left [l] <= right [r]){
-	             data [i] = left [l];
+	             original [i] = left [l];
 	             l ++;
 				 i ++;
 	        }
-			System.out.println("r" + r + "length " + right.length );
-			System.out.println("ll" + l+ "length " + left.length );
-			if ( l < left.length && r < right.length && i < data.length && left [l] > right [r]){
-	             data [i] = right [r];
+			if ( l < left.length && r < right.length && i < original.length && left [l] > right [r]){
+	             original [i] = right [r];
 	             r ++;
 				 i ++;
 	        }
 		}
-	while (r < right.length && i < data.length){
-			data [i] = right [r];
+	while (r < right.length && i < original.length){
+			original [i] = right [r];
 		   	r ++;
 			i ++;
 	  	}
-	while (l < right.length && i < data.length){
-			data [i] = right [l];
+	while (l < left.length && i < original.length){
+			original [i] = left [l];
 		   	l ++;
 			i ++;
 	  	}
-
 		}
+
 	}
